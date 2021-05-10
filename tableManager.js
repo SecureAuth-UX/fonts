83102fd66c7068da28ae1a1b34968e8ccf5cf6e8
SecureAuth <58443272+SecureAuth-UX@users.noreply.github.com>
2021-05-10 13:59:39 -0300

    Update tableManager.js

--- tableManager.js
+++ tableManager.js
@@ -170,7 +170,7 @@
                     translate(voc_show_rows) +
                     ': </label><select id="numrows"></select></div>';
                 // append div to choose num rows to show
-                TFooter.append(numrowsDiv);
+                TFooter.html(numrowsDiv);
                 // get show rows options and append select to its div
                 for (i = 0; i < showrows.length; i++) {
                     $("select#numrows").append(
@@ -210,7 +210,7 @@
                 // append num pages on bottom
                 var pagesDiv =
                     '<div id="pagesControllers" class="pagesControllers"></div>';
-                TFooter.append(pagesDiv);
+                TFooter.html(pagesDiv);
 
                 // Showrows option and append
                 // If showrows is set get select val
