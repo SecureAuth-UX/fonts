c2cdaa5873ac190aae49f518f66ea73edbdbf775
SecureAuth <58443272+SecureAuth-UX@users.noreply.github.com>
2021-05-20 16:41:48 -0300

    Update tableManager.js

--- tableManager.js
+++ tableManager.js
@@ -257,10 +257,9 @@
             ) {
                 // Create div and select to filter
                 // var filterbyDiv =
-                //     '<div id="for_filter_by" class="for_filter_by" style="display: inline;">' +
-                //     '<input id="filter_input" type="text" placeholder="'+translate(voc_type_here_filter) +'">' +
-                //     '<label for="filter_by">'+translate(voc_filter_by)+': </label>'+
-                //     '<select id="filter_by"></select> </div>';
+                //     '<div id="for_filter_by" class="for_filter_by" style="display: inline;"><label for="filter_by">' +
+                //     translate(voc_filter_by) +
+                //     ': </label><select id="filter_by"></select> </div>';
                 // $(this).before(filterbyDiv);
 
                 // Populate select with every th text and as value use column number
@@ -269,7 +268,6 @@
                         $("ul#filter_by").append(
                             $("<li>", {
                                 value: i,
-                                class: "sa-form-select-custom-option",
                                 text: $(this).text(),
                             })
                         );
